d277adccfaa3ad37a9186a95292d55f510ef0063
jeongbeen <binibin0122@gmail.com>
2018-04-23 20:03:49 +0900

    first commit

---
+++ CodeReviewProgram/Forms/InfoInput.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace CodeReviewProgram.Forms
+{
+    partial class InfoInput
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.infoInputControl1 = new CodeReviewProgram.Controls.InfoInputControl();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // infoInputControl1
+            // 
+            this.infoInputControl1.Location = new System.Drawing.Point(12, 12);
+            this.infoInputControl1.Name = "infoInputControl1";
+            this.infoInputControl1.Size = new System.Drawing.Size(275, 162);
+            this.infoInputControl1.TabIndex = 0;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(70, 180);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(55, 55);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "확인";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(178, 180);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(55, 55);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "취소";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // InfoInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 241);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.infoInputControl1);
+            this.Name = "InfoInput";
+            this.Text = "InfoInput";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Controls.InfoInputControl infoInputControl1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ CodeReviewProgram/Forms/Login.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace CodeReviewProgram.Forms
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.loginControl1 = new CodeReviewProgram.Controls.LoginControl();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 66);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "로그인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // loginControl1
+            // 
+            this.loginControl1.Location = new System.Drawing.Point(12, 9);
+            this.loginControl1.Name = "loginControl1";
+            this.loginControl1.Size = new System.Drawing.Size(183, 89);
+            this.loginControl1.TabIndex = 5;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 105);
+            this.Controls.Add(this.loginControl1);
+            this.Controls.Add(this.button1);
+            this.Name = "Login";
+            this.Text = "로그인";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private Controls.LoginControl loginControl1;
+    }
+}
